120cea6ea06bcaa5e80767a03b9b24dd2a967223
GaYoung <kians2@naver.com>
2020-10-11 16:39:04 +0900

    seminar

--- 1st-seminar/symbol.js
+++ 1st-seminar/symbol.js
@@ -17,13 +17,3 @@ console.log(arr.includes(1));  // js 기본 함수
 console.log(arr['includes'](1)); // js 기본 함수
 console.log(arr[includes](1));  // 커스텀 함수
 
-
-// null, undefined
-let nothing = null;
-console.log(`nothing : ${nothing}, type : ${typeof nothing}`);
-let x;
-console.log(` x : ${x}, type ${typeof x}`);
-
-console.log('null vs undefiend');
-console.log('null == undefined', null == undefined);  // type 은 비교 안하고 값만 비교한다
-console.log('null === undefined', null === undefined); // 동등연산자 : type도 비교한다.

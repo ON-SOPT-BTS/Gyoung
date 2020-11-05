c539ae6eca73e8aa44cbc4c61e60c396edbdb2ac
GaYoung <kians2@naver.com>
2020-11-06 03:58:07 +0900

    3주차 과제 완료

--- 3rd-seminar/routes/users/index.js
+++ 3rd-seminar/routes/users/index.js
@@ -7,57 +7,88 @@ const statusCode = require('../../modules/statusCode');
 let usersDB = require('../../modules/users');
 
 router.post('/signup', async (req, res) => {
-  const {id, password} = req.body; // 1. req.body에서 데이터 가져오기
-  //2. request data 확인하기, id 또는 password data가 없다면 NullValue 반환
-  if (!id || !password){
-      return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
-  }
-  //3. 존재하는 아이디인지 확인하기. 이미 존재하는 아이디면 ALREADY ID 반환
-  const isIddOverlap = newId => {
-      for(let i = 0; i < usersDB.length; i++){
-          if (usersDB[i] == newId){
-              return true
-          }
-      }
-      return false
-  }
-  if (isIddOverlap(id)){
-      return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.ALREADY_ID));
-  }
-  const createSalt = async () => {
-      crypto.randomBytes(64, (err, buf) => {
-      //4. salt 생성
-      const salt = buf.toString('base64');
-      return salt;
-  });
-}
-    const salt = await createSalt();
-    //5. 2차 세미나때 배웠던 pbkdf2 방식으로 (비밀번호 + salt) 해싱하여 => 암호화된 password 를 만들기!
-    crypto.pbkdf2(password, salt, 100000, 64, 'sha512', (err, key) => {
-        console.log(`password: ${key.toString('base64')}`)
-        password = key.toString
-    })
-    //6. usersDB에 id, 암호화된 password, salt 저장!
-    usersDB.push({
-        id,
-        password,
-        salt
-    });
-    console.log(usersDB);
-    //7. status: 200 message: SING_UP_SUCCESS, data: id만 반환! (비밀번호, salt 반환 금지!!)
-    res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.SIGN_UP_SUCCESS, id))    
+    try{
+        const {id, password} = req.body; // 1. req.body에서 데이터 가져오기
+        //2. request data 확인하기, id 또는 password data가 없다면 NullValue 반환
+        if (!id || !password){
+            return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+        }
+        //3. 존재하는 아이디인지 확인하기. 이미 존재하는 아이디면 ALREADY ID 반환
+        const isIddOverlap = newId => {
+            for(let i = 0; i < usersDB.length; i++){
+                if (usersDB[i].id == newId){
+                    return true
+                }
+            }
+            return false
+        }
+        if (isIddOverlap(id)){
+            return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.ALREADY_ID));
+        }
+        //4. salt 생성
+        const salt = await new Promise((resolve, reject) => {
+            crypto.randomBytes(64, (err, buf) => {
+                const salt = buf.toString('base64');
+                resolve(salt);
+            })
+        })
+        //5. 2차 세미나때 배웠던 pbkdf2 방식으로 (비밀번호 + salt) 해싱하여 => 암호화된 password 를 만들기!
+        const newPassword = await new Promise((resolve, reject) => {
+            crypto.pbkdf2(password, salt, 100000, 64, 'sha512', (err, key) => {
+                if (err){
+                    reject(err);
+                }
+                resolve(key.toString('base64'));
+            })
+        })
+          //6. usersDB에 id, 암호화된 password, salt 저장!
+          usersDB.push({
+              id,
+              password : newPassword,
+              salt
+          });
+          console.log(usersDB);
+          //7. status: 200 message: SING_UP_SUCCESS, data: id만 반환! (비밀번호, salt 반환 금지!!)
+          res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.SIGN_UP_SUCCESS, {id})) ;
+    }catch(err) {
+        console.error(err);
+    }
+     
 })
 
-router.post('/signin', (req, res) => {
+router.post('/signin', async (req, res) => {
   const {id, password} = req.body; // 1. req.body에서 데이터 가져오기
   //2. request data 확인하기, id 또는 password data가 없다면 NullValue 반환
+  if (!id || !password) {
+      return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+  }
   //3. 존재하는 아이디인지 확인하기. 존재하지 않는 아이디면 NO USER 반환
+  const user = await new Promise((resolve, reject) => {
+      resolve(usersDB.filter(m => m.id == id));
+  })
+  if (user.length == 0) {
+    return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NO_USER));
+  }
   //4. 비밀번호 확인하기 - 로그인할 id의 salt를 DB에서 가져와서  사용자가 request로 보낸 password와
   //   암호화를 한후 디비에 저장되어있는 password와 일치하면 true일치하지 않으면 Miss Match password 반환
+  const newPassword = await new Promise((resolve, reject) => {
+      crypto.pbkdf2(password, user[0].salt, 100000, 64, 'sha512', (err, key) => {
+          if (err) {
+              reject(err);
+          }
+          resolve(key.toString('base64'));
+      })
+  })
+  
+  if (newPassword != user[0].password) {
+      return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.MISS_MATCH_PW));
+  }
   //5. status: 200 ,message: SIGNIN SUCCESS, data: id 반환 (비밀번호, salt 반환 금지!!)
+  res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.SIGN_IN_SUCCESS, {id}));
 })
 router.get('/', (req, res) => {
   // 1.모든 유저정보 조회 (id, password, salt)!
+  res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.MEMBER_READ_SUCCESS, usersDB));
 })
 
 module.exports = router;

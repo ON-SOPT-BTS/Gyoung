51fa0bd8478066fe530513b4362c7c842d0417d6
GaYoung <kians2@naver.com>
2020-11-06 17:52:52 +0900

    3주차

--- 3rd-seminar/routes/index.js
+++ 3rd-seminar/routes/index.js
@@ -1,16 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-
-io.on('connection', function(socket){
-    console.log('a user connected');
-    socket.on('from client', function(msg) {
-        console.log('message: ' + msg);
-        io.emit('to client', msg);
-    })
-})
 
 router.use('/users', require('./users'));
 router.use('/ranking', require('./ranking'));
